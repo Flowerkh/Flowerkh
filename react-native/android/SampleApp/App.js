a317dd4a9a0669b53606951cf1ccc3274f9f69c7
kyungHa <cdffee1@naver.com>
2021-06-21 14:20:58 +0900

    update SampleApp

--- react-native/android/SampleApp/App.js
+++ react-native/android/SampleApp/App.js
@@ -1,112 +1,66 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+import React, {Fragment, Component, useState} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  Text,
-  useColorScheme,
+  ScrollView,
   View,
+  Text,
+  Image,
+  Button,
+  StatusBar,
 } from 'react-native';
-
 import {
-  Colors,
-  DebugInstructions,
   Header,
   LearnMoreLinks,
+  Colors,
+  DebugInstructions,
   ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+} from 'react-native/Libraries/NewAppScreen'
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+//Main 함수
+const App = () => {
+  const [isPressed, setIsPressed] = useState(true);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+      <View style={styles.container}>
+        <Text style={styles.redText}>Hello, world!!!</Text>
+        <Photo type='sleep'></Photo>
+        <Photo type='front'></Photo>
 
+        <Button title={'나의 이름 출력'} onPress={()=>{setIsPressed(false)}}/>
+        <Button title={'리셋'} onPress={()=>{setIsPressed(true)}}/>
+        <Text> {isPressed ? '' : '해위~ 난 두식'} </Text>
+      </View>
+  )
+}
+
+//style 함수
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  redText: {
+    color: 'red',
+  }
 });
+
+//Photo 클래스
+function Photo(props) {
+  let dolImg = '';
+  if(props.type==='sleep') {
+    dolImg=require('./assets/sleep.jpg');
+  } else if(props.type==='front') {
+    dolImg=require('./assets/front.jpg');
+  }
+  return (
+      <View>
+        <Image
+            source={dolImg}
+            style={{width:200, height:200}}/>
+      </View>
+  )
+}
 
 export default App;
---
+++ react-native/android/SampleApp/MyApp.js
@@ -0,0 +1,10 @@
+import React, { Component} from 'react';
+import {Text} from 'react-native';
+
+class MyApp extends Component{
+    render(){
+        return <Text>Hello ReactNative</Text>
+    }
+}
+
+export default MyApp;
