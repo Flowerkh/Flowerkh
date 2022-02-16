12cdcf28b7e25c32fd53b64e41928b2e320b9885
KyungHa Kim <32595112+Flowerkh@users.noreply.github.com>
2022-02-16 16:43:20 +0900

    Create doubleSubmitFlag.js

---
+++ js/doubleSubmitFlag.js
@@ -0,0 +1,15 @@
+var doubleSubmitFlag = false;
+function doubleSubmitCheck(){
+    if(doubleSubmitFlag){
+        return doubleSubmitFlag;
+    }else{
+        doubleSubmitFlag = true;
+        return false;
+    }
+}
+
+function insert() {
+  if(doubleSubmitCheck()) return;
+
+  alert("등록");
+}
