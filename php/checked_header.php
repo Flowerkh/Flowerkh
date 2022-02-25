37d460e7d9dad4c61cd9a71b3f31d6f5dda28456
KyungHa Kim <32595112+Flowerkh@users.noreply.github.com>
2022-02-25 10:33:52 +0900

    Update checked_header.php

--- php/checked_header.php
+++ php/checked_header.php
@@ -1,15 +1,23 @@
-    private function get_headers_info($url)
+//function
+private function get_headers_info($url)
+{
+    $rtn = array();
+
+    $rtn_headers = get_headers($url);
+
+    foreach($rtn_headers as $vl)
     {
-        $rtn = array();
-
-        $rtn_headers = get_headers($url);
-
-        foreach($rtn_headers as $vl)
-        {
-            $tm_vl = explode(' ', $vl);
-            if( array_search('HTTP/1.1', $tm_vl) !== false ) $rtn['status'] = trim($tm_vl[1]);
-            if( array_search('Content-Type:', $tm_vl) !== false ) $rtn['type'] = trim($tm_vl[1]);
-        }
-
-        return $rtn['status'];
+        $tm_vl = explode(' ', $vl);
+        if( array_search('HTTP/1.1', $tm_vl) !== false ) $rtn['status'] = trim($tm_vl[1]);
+        if( array_search('Content-Type:', $tm_vl) !== false ) $rtn['type'] = trim($tm_vl[1]);
     }
+
+    return $rtn['status'];
+}
+
+//use
+if($this->get_headers_info($img_url) == 200)
+{
+    $image_path = $img_url;
+    break;
+}
